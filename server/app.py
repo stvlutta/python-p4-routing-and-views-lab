2afc4814415401992b0dfee0e9752eaf5f012734
Stvlutta <stvlutta@gmail.com>
2025-06-09 14:46:31 +0300

    Implement Flask routing and views lab requirements
    - Add index route at '/' with h1 title
    - Add print_string route at '/print/<parameter>' with console output
    - Add count route at '/count/<parameter>' displaying range
    - Add math route at '/math/<num1>/<operation>/<num2>' supporting +, -, *, div, %

--- server/app.py
+++ server/app.py
@@ -4,5 +4,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    result = []
+    for i in range(parameter):
+        result.append(str(i))
+    return '\n'.join(result) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(float(num1 / num2))
+    elif operation == '%':
+        return str(num1 % num2)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
